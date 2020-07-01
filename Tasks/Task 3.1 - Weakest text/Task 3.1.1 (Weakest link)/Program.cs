38ed99a9b6c374d20539dcab52cde6af90891b88
Александр <universe.is.portable@gmail.com>
2020-07-01 18:29:45 +0400

    Completed all tasks 3.1

---
+++ Tasks/Task 3.1 - Weakest text/Task 3.1.1 (Weakest link)/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._1._1__Weakest_link_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int N;
+            int position;
+
+            Console.Write("Enter the number of people: ");
+            var number = Console.ReadLine();
+            Console.Write("Enter which person's score will be crossed out each round: ");
+            var numeric = Console.ReadLine();
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="number"></param>
+            /// <param name="numeric"></param>
+            /// <exception cref="System.FormatException">Thrown when user input is incorrect</exception>
+            /// <returns></returns>
+            try
+            {
+                N = CheckingСonversion(number);
+                position = CheckingСonversion(numeric);
+            }
+            catch (InvalidCastException ex)
+            {
+                throw new FormatException("User input is incorrect", ex);
+            }
+
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="position"></param>
+            /// <param name="N"></param>
+            /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the "position" parameter is greater than the "N" parameter</exception>
+            /// <returns></returns>
+            if (position > N)
+            {
+                throw new ArgumentOutOfRangeException("position", "The number of the person being crossed out cannot be more than the number of people");
+            }
+
+            List<int> list = new List<int>(N);
+            Console.WriteLine("\nAll people who play counting games: ");
+            while (index < N)
+            {
+                list.Add(index + 1);
+                Console.Write($"{list[index]}, ");
+                index += 1;
+            }
+            Console.WriteLine();
+
+            index = 0;
+
+            while (position <= N)
+            {
+                if (position > N)
+                {
+                    Console.WriteLine("\n\nThe game is over. You can't cross out more people.");
+                    break;
+                }
+                list.RemoveAt(position - 1);
+                Console.Write($"\nRound {index += 1}. A person is crossed out. People left: {N -= 1}.");
+                position += position;
+            }
+
+            Console.WriteLine("\n\nPeople who are not crossed out: ");
+            foreach (var item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <exception cref="System.InvalidCastException">Thrown when user input cannot convert to int</exception>
+        /// <returns></returns>
+        public static int CheckingСonversion(string userInput)
+        {
+            if (int.TryParse(userInput, out int value) == !false)
+            {
+                return value;
+            }
+            else
+            {
+                throw new InvalidCastException("Invalid input! Cannot convert to int!");
+            }
+        }
+    }
+}
---
+++ Tasks/Task 3.1 - Weakest text/Task 3.1.2 (Text analysis)/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._1._2__Text_analysis_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            bool flag = false;
+            int numberRepeated = 1;
+            int maxRepetitions = -1;
+
+            Console.WriteLine("I am very happy to welcome you to my program. Let's check your speech for monotony and find a \"favorite\" words. " +
+                "Write me something:\n");
+            string str = Console.ReadLine();
+
+            string[] split = str.Split(new char[] {' ', '.', ',', '?', '!', ':', ';'}, StringSplitOptions.RemoveEmptyEntries);
+            string[] prepositionsAndKeywords = new string[] { "a", "the", "of", "at", "on", "in", "am", "is", "are", "by", "for" };
+
+            List<int> number = new List<int>(split.Length);
+            List<string> list = new List<string>(split.Length);
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                foreach (var item in prepositionsAndKeywords)
+                {
+                    if (split[i].ToLower() == item)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag)
+                {
+                    list.Add(split[i]);
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            
+            Console.WriteLine("\nThe number of repetitions of each word from the text you entered: ");
+            for (int i = 0; i < list.Count(); i++)
+            {
+
+                for (int j = i + 1; j < list.Count(); j++)
+                {
+                    if (list[i].ToString().ToLower() == list[j].ToString().ToLower())
+                    {
+                        numberRepeated++;
+                        list.RemoveAt(j);
+                        j--;
+                    }
+                }
+
+                if (maxRepetitions < numberRepeated)
+                {
+                    maxRepetitions = numberRepeated;
+                }
+
+                number.Add(numberRepeated);
+                numberRepeated = 1;
+
+                Console.WriteLine($"{list[i]} = {number[i]}");
+            }
+
+            foreach (var item in number)
+            {
+                if (item != maxRepetitions)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                Console.WriteLine("\nYour \"favorite\" words:");
+                for (int i = 0; i < number.Count(); i++)
+                {
+                    if (number[i] == maxRepetitions)
+                    {
+                        count++;
+                        Console.Write($"{list[i]}, ");
+                    }
+                }
+                Console.WriteLine($"you repeated them {maxRepetitions} times");
+
+                if (count > 2 || maxRepetitions > 2)
+                {
+                    Console.WriteLine("\nYour speech is too monotonous. Try to use your \"favorite\" words less often.");
+                }
+                else
+                {
+                    Console.WriteLine("\nCongratulations!!! Your speech is diverse!");
+                }
+
+            }
+            else if (maxRepetitions > 2)
+            {
+                Console.WriteLine("\nWe didn't find your \"favorite\" word. All the words were repeated the same number of times, " +
+                    "but your speech is still very monotonous. Try to avoid tautology.");
+            }
+            else
+            {
+                Console.WriteLine("\nWe didn't find your \"favorite\" word. All the words were repeated the same number of times, " +
+                    "but your speech is diverse. Congratulations!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
